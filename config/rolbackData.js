fb1dbe5d0ef2d61a96dae72e4c72a44092afc59b
jherrera <jherrera@makita.cl>
2024-07-24 17:49:46 -0400

    solo falta llamar al que crea documento y modificar el sp para que ,muestre el documento en la puesta en marcha

--- config/rolbackData.js
+++ config/rolbackData.js
@@ -1,25 +1,20 @@
 const sql = require('mssql');
 const { connectToDatabase, closeDatabaseConnection } = require('./database.js');
+const logger = require('../config/logger.js');
 
 async function rolbackData(data) {
     try {
-        console.log("dataaaaaa: ", data);
+       logger.info(`Iniciamos proceso de rollback`);
          let result;
         // Conectarse a la base de datos 'BdQMakita'
         await connectToDatabase('Telecontrol');
-       
-        for(id of data){
-
-            const request = new sql.Request(); // Nueva instancia de request en cada iteración
-       
-            result = await request
-                .input('ID', sql.VarChar, id.resultadoID.toString())
+        const request = new sql.Request(); // Nueva instancia de request en cada iteración
+        result = await request
+                .input('ID', sql.VarChar, data.toString())
                 .execute('Telecontrol.dbo.BorrarDatosPuestaMarcha');
                 
-
-        }
-        
-        console.log("resultado rolbackData : " , result);
+                logger.info(`Se elimina orden de servicio N° ${data}`);
+                logger.info(`Fin del proceso rollback`);
     } catch (err) {
         console.error('Error al borrar data mediante el procedimiento almacenado BorrarDatos:', err.message);
     } finally {
